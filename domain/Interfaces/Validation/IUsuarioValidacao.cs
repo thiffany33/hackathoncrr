610038158b7885f4ee0a5450c9a0b9bed6dc877a
Thiffany Gabrielle Da Silva Gonçalves <thiffanygabrielle48@gmail.com>
2021-01-15 17:41:36 -0300

    UsuarioService foi adicionado e está sem erros

--- domain/Interfaces/Validation/IUsuarioValidacao.cs
+++ domain/Interfaces/Validation/IUsuarioValidacao.cs
@@ -1,6 +1,13 @@
-﻿namespace Domain.Interfaces.Validation
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Domain.Interfaces.Validation
 {
     public interface IUsuarioValidacao
     {
+        public List<string> ValidarCadastrar(Usuario usuario);
+        public List<string> ValidarEditar(Usuario usuario);
+        public List<string> ValidarDeletar(Guid id);
     }
 }
---
+++ hackathoncrr.Aplication/Services/UsuarioService.cs
@@ -0,0 +1,52 @@
+﻿using Aplication.Interfaces;
+using Domain.Entities;
+using Domain.Interfaces;
+using Domain.Interfaces.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aplication.Services
+{
+    public class UsuarioService : IUsuarioService
+    {
+        private readonly IUsuarioRepository _usuarioRepository;
+        private readonly IUsuarioValidacao _usuarioValidacao;
+
+        public UsuarioService(IUsuarioRepository usuarioRepository, IUsuarioValidacao usuarioValidacao)
+        {
+            _usuarioRepository = usuarioRepository;
+            _usuarioValidacao = usuarioValidacao;
+        }
+
+        public List<string> Cadastrar(Usuario usuario)
+        {
+            var erros = _usuarioValidacao.ValidarCadastrar(usuario);
+            if (erros.Count() == 0)
+            {
+                _usuarioRepository.Create(usuario);
+            }
+            return erros;
+        }
+
+        public List<string> Editar(Usuario usuario)
+        {
+            var erros = _usuarioValidacao.ValidarEditar(usuario);
+            if(erros.Count() == 0)
+            {
+                _usuarioRepository.Update(usuario);
+            }
+            return erros;
+        }
+
+        public List<string> Deletar(Guid id)
+        {
+            var erros = _usuarioValidacao.ValidarDeletar(id);
+            if (!erros.Any())
+            {
+                _usuarioRepository.Delete(id);
+            }
+            return erros;
+        }
+    }
+}
