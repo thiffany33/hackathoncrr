f14456c19b8f7532110871fef90172ace7db9da8
Thiffany Gabrielle Da Silva Gonçalves <thiffanygabrielle48@gmail.com>
2021-01-15 17:48:52 -0300

    UsuarioController foi adicionado e está funciuonando

--- hackathoncrr.Aplication/Interfaces/IUsuarioService.cs
+++ hackathoncrr.Aplication/Interfaces/IUsuarioService.cs
@@ -1,6 +1,13 @@
-﻿namespace Aplication.Interfaces
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Aplication.Interfaces
 {
     public interface IUsuarioService
     {
+        public List<string> Deletar(Guid id);
+        public List<string> Editar(Usuario usuario);
+        public List<string> Cadastrar(Usuario usuario);
     }
 }
---
+++ hackathoncrr/Controllers/UsuarioController.cs
@@ -0,0 +1,56 @@
+﻿using Aplication.Interfaces;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("Usuario")]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioService _usuarioService;
+
+        public UsuarioController(IUsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+        [HttpPost]
+        [Route("Cadastrar")]
+        public ActionResult Cadastrar(Usuario usuario)
+        {
+            var erros = _usuarioService.Cadastrar(usuario);
+            if (erros.Count() > 0)
+            {
+                return BadRequest(erros);
+            }
+            return Ok("Cadastrado com Sucesso");
+        }
+
+        [HttpPut]
+        [Route("Editar")]
+        public ActionResult Editar(Usuario usuario)
+        {
+            var erros = _usuarioService.Editar(usuario);
+            if (erros.Count() > 0)
+            {
+                return BadRequest(erros);
+            }
+            return Ok("Atualizado com Sucesso");
+        }
+
+        [HttpDelete]
+        [Route("Deletar")]
+        public ActionResult Deletar(Guid id)
+        {
+            var erros = _usuarioService.Deletar(id);
+            if (erros.Any())
+            {
+                return BadRequest(erros);
+            }
+            return Ok("Deletado com Sucesso");
+        }
+    }
+}
