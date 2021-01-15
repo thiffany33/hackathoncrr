05a5f3afcd7510b9ea4957395d618c7f89476c3e
Thiffany Gabrielle Da Silva Gonçalves <thiffanygabrielle48@gmail.com>
2021-01-15 17:32:00 -0300

    UsuarioMapping foi adicionado e está funcionando

---
+++ Infra/Mapping/UsuarioMapping.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infra.Mapping
+{
+    public class UsuarioMapping : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.HasKey(q => q.Id);
+            builder.Property(q => q.Nome).IsRequired().HasMaxLength(500);
+            builder.Property(q => q.Telefone).IsRequired().HasMaxLength(20);
+            builder.Property(q => q.Email).IsRequired().HasMaxLength(150);
+            builder.Property(q => q.Renda).IsRequired();
+        }
+    }
+}
