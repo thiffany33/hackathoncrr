c5a7347902bcdbbc34abef34cd0d7705ee7b221c
Thiffany Gabrielle Da Silva Gonçalves <thiffanygabrielle48@gmail.com>
2021-01-16 19:07:54 -0300

    EmpresaEntitie e EmpresaMapping foram adicionado

---
+++ Infra/Mapping/EmpresaMapping.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infra.Mapping
+{
+    public class EmpresaMapping : IEntityTypeConfiguration<Empresa>
+    {
+        public void Configure(EntityTypeBuilder<Empresa> builder)
+        {
+            builder.HasKey(q => q.Id);
+            builder.Property(q => q.Pessoa).IsRequired();
+            builder.Property(q => q.RazaoSocial).IsRequired().HasMaxLength(300);
+            builder.Property(q => q.Email).IsRequired().HasMaxLength(200);
+            builder.Property(q => q.Telefone).IsRequired();
+            builder.Property(q => q.CNPJ).IsRequired().HasMaxLength(15);
+        }
+    }
+}
---
+++ domain/Entities/Empresa.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Entities
+{
+    public class Empresa : BaseEntitie
+    {
+        public string RazaoSocial { get; set; }
+
+        public Pessoa Pessoa { get; set; }
+
+        public string CNPJ { get; set; }
+
+        public string Email { get; set; }
+
+        public string Telefone { get; set; }
+    }
+}
